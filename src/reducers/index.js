ec5ebf70a14d22965dfdb9b2a714b30566026224
julien-gabard <juliengabard@hotmail.fr>
2021-07-06 06:17:12 +0200

    Create article reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import userReducer from './user.reducer';
+import articleReducer from './article.reducer';
 
 const rootReducer = combineReducers({
   user: userReducer,
+  article: articleReducer,
 });
 
 export default rootReducer;
